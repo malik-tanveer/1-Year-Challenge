ac90e7b55ba53e2ce7221c6782c34376e5666551
tanveer-developer123 <youloos477@gmail.com>
2025-12-21 18:45:51 +0500

    Add React component structure for Day 2 and Day 3
    Refactored Day 2 App.jsx to use a new Header and Home component, and added Home.jsx. Initialized new React + Vite projects for Day 3 and Day 4_Day_7 with basic setup files, ESLint configuration, and documentation. Added related PDF resources and .gitignore files.

--- Phase 2/Dec/Week_1/Day_2/src/App.jsx
+++ Phase 2/Dec/Week_1/Day_2/src/App.jsx
@@ -1,24 +1,18 @@
-import { useState } from 'react'
+import Home from "./components/Home"
+
+function Header() {
+  return <h2>Hello i am tanveer a Full stcak</h2>
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <Home/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ Phase 2/Dec/Week_1/Day_2/src/components/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+        <>
+        Hello this is a Home Page can you see this what a work 
+        </>
+    )
+}
+
+export default Home;
