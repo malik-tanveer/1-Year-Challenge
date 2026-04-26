525a62d9606a1af37de139b46325bbd87e57649e
tanveer-developer123 <197722550+malik-tanveer@users.noreply.github.com>
2026-04-26 14:17:00 +0500

    Leanr a How to make a Routes in nextjs backend

---
+++ Phase 3/March/Week_2/server-client/src/app/api/posts/route.js
@@ -0,0 +1,53 @@
+export async function GET() {
+    return Response.json([
+        {
+            id: 1,
+            title: "post is a 1",
+            des : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+            created_at : "2023-01-01",
+            platofrm : "Facebook"
+        },
+        {
+            id : 2,
+            title: "post is a 2",
+            des : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+            created_at : "2023-01-01",
+            platofrm : "Whatsapp"
+        },
+        {
+            id : 3,
+            title: "post is a 3",
+            des : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+            created_at : "2023-01-01",
+            platofrm : "Instagram"  
+        },
+        {
+            id : 4,
+            title : "post is a 4",
+            des : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+            created_at : "2023-01-01",
+            platofrm : "Twitter"
+        },
+        {
+            id : 5,
+            title : "post is a 5",
+            des : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+            created_at : "2023-01-01",
+            platofrm : "LinkedIn"
+        },
+        {
+            id : 6,
+            title : "post is a 6",
+            des : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+            created_at : "2023-01-01",
+            platofrm : "Reddit"
+        },
+        {
+            id : 7,
+            title : "post is a 7",
+            des : "Loream ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+            created_at : "2023-01-01",
+            pltofrm : ""
+        }
+    ])
+}
